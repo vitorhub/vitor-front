68c07568900dba1e3f8f3854bff2afe10c6025b8
Vitor Falcao <joaofalcao.33@outlook.com>
2023-06-09 13:26:54 -0300

    creating form to backend

--- src/components/ComponenteB.js
+++ src/components/ComponenteB.js
@@ -3,9 +3,6 @@
 import React, { Component, useState } from "react";
 import axios, { isCancel, AxiosError } from "axios";
 
-export const sum = (a, b) => {
-  return a + b;
-};
 const ComponenteB = () => {
   let url = "http://localhost:3001/user/auth/login";
 
@@ -16,7 +13,6 @@ const ComponenteB = () => {
     console.log(response);
     // JSON.stringfy(data)
   }) */
-
   function login() {
     axios
       .post(url, {
@@ -28,17 +24,22 @@ const ComponenteB = () => {
       })
       .catch((error) => console.log(error));
   }
+
+  const [stateCria, setStateCria] = useState({
+    name: "",
+    email: "",
+    password: "",
+    repeatpassword: "",
+  });
+  // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
   function cria() {
-    axios.post("http://localhost:3001/user/", {
-        name: "vitor beina",
-        email: "joaofalcao.35@outlook.com",
-        password: "123456",
-        repeatpassword: "123456",
-      })
+    axios
+      .post("http://localhost:3001/user/", stateCria)
       .then((response) => {
-        console.log(response)
+        console.log(response);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error.response.data));
   }
 
   return (
@@ -54,22 +55,49 @@ const ComponenteB = () => {
         </button>
         <p> {tokenId.token}</p> <br />
         <p>{tokenId.id}</p> <br />
-        <p>{sum(2, 2)}</p>
       </div>
-      
-      <button onClick={()=>cria()}>cria usuario</button>
-
-      <form action="http://localhost:3001/user/" method="POST">
+      <button onClick={() => cria()}>cria usuario</button>
+      <form onSubmit={()=>cria()}>
         <label htmlFor="name">name</label>
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          onChange={(e) => setStateCria({ ...stateCria, name: e.target.value })}
+          value={stateCria.nome}
+        />
         <label htmlFor="email">email</label>
-        <input type="text" name="email" />
+        <input
+          type="text"
+          name="email"
+          onChange={(e) =>
+            setStateCria({ ...stateCria, email: e.target.value })
+          }
+          value={stateCria.email}
+        />
         <label htmlFor="password">password</label>
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          onChange={(e) =>
+            setStateCria({ ...stateCria, password: e.target.value })
+          }
+          value={stateCria.password}
+        />
         <label htmlFor="repeatpassword">repeat</label>
-        <input type="password" name="repeatpassword" />
+        <input
+          type="password"
+          name="repeatpassword"
+          onChange={(e) =>
+            setStateCria({ ...stateCria, repeatpassword: e.target.value })
+          }
+          value={stateCria.repeatpassword}
+        />
         <input type="submit" value={"enviar"} />
       </form>
+      {stateCria.name} <br />
+      {stateCria.email} <br />
+      {stateCria.password} <br />
+      {stateCria.repeatpassword}
     </>
   );
 };
